b5e31bfe1ddd096da9ea62efedf155256df3c94d
Jack Long <putheara.long@gmail.com>
2020-02-23 19:13:39 -0500

    got password to generate, but not the desired length

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+const minLength = 8;
+const maxLength = 128;
+var desiredLength = parseInt(prompt("Please enter your desired lenght for the password!"), 10);
+
+
+randomPass = "";
+
+if (desiredLength >= minLength && desiredLength <= maxLength) {
+  var max = 4;
+  for (var i= 1; i<= desiredLength; i++) {
+    
+    function getRandomInt() {
+      return Math.floor(Math.random() * Math.floor(max));
+    };
+
+    if (getRandomInt() === 0){
+    randomPass += randomLowercase()}
+
+    else if (getRandomInt() === 1){
+      randomPass += randomUppercase()}
+
+    else if (getRandomInt() === 2){
+      randomPass += randomNumber()}
+
+    else if (getRandomInt() === 3){
+      randomPass += randomSymbol()}
+  }
+
+};
+  
+  
+
+console.log(randomPass);
+//var disredSymbols = prompt("Please enter if you desire special characters in the password!")
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -14,30 +49,27 @@ function randomLowercase(){
   const lowercaseList = "abcdefghijklmnopqrstuvwxyz";
   return lowercaseList[Math.floor(Math.random() * lowercaseList.length)];  
   
-}
+};
 
 function randomUppercase(){
   const uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return uppercaseList[Math.floor(Math.random() * uppercaseList.length)];  
   
-}
+};
 
 function randomNumber(){
   const numbersList = "0123456789";
   return numbersList[Math.floor(Math.random() * numbersList.length)];  
   
-}
+};
 
 function randomSymbol(){
   const symbolsList = "!@#$%&*_-?/~";
   return symbolsList[Math.floor(Math.random() * symbolsList.length)];  
   
-}
+};
 
+console.log(randomPass.length)
 
-console.log(randomLowercase());
-console.log(randomUppercase());
-console.log(randomNumber());
-console.log(randomSymbol());
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
