729d78d0f16540fede531acaf8ece231a7c24dc2
Jack Long <putheara.long@gmail.com>
2020-02-24 23:03:59 -0500

    committing final product

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # homework-week3
+
+this code generates random password with lowercase, uppercase, numbers and special characters.
+
+lowercase letters are set to default in the sense that if upper case, numbers, and special characters are not needed, the password would be generated entirely of lowercase letters.
+
+i can stop uppercase, numbers, and special characters from appearing in the password, but i cannot ensure that at least 1 of any of the 4 types of characters appear in the password even if they are selected.
+
+the list of special characters in the code are the ones i selected.  characters can be added or removed from the list as desired.
+
+the good news is that the code spits out the correct length.
+
+the longer the length the more chance at least one of the desired type of character would appear in the password.
+
+do long password or generated the password a few times to get the desired result.
--- script.js
+++ script.js
@@ -1,75 +1,53 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-const minLength = 8;
-const maxLength = 128;
-var desiredLength = parseInt(prompt("Please enter your desired lenght for the password!"), 10);
-
+var desiredLength = parseInt(prompt("Please enter your desired lenght between 8 and 128 characters for the password!"), 10);
+var upperDesired = confirm("Do you want uppercase letters in the password?")
+var numberDesired = confirm("Do you want numbers in the password?")
+var symbolDesired = confirm("Do you want special characters in the password?")
 
 randomPass = "";
 
-if (desiredLength >= minLength && desiredLength <= maxLength) {
-  var max = 4;
-  for (var i= 1; i<= desiredLength; i++) {
-    
-    function getRandomInt() {
-      return Math.floor(Math.random() * Math.floor(max));
-    };
+if (desiredLength >= 8 && desiredLength <= 128) {
+   
+  function getRandomInt() {
+    return Math.floor(Math.random() * Math.floor(4));
+  }
+    for (i = 1; i <= desiredLength; i++){
+      if (getRandomInt() === 0 && symbolDesired === true){
+        randomPass += randomSymbol()}
 
-    if (getRandomInt() === 0){
-    randomPass += randomLowercase()}
+      else if (getRandomInt() === 1 && upperDesired === true){
+        randomPass += randomUppercase()}
 
-    else if (getRandomInt() === 1){
-      randomPass += randomUppercase()}
-
-    else if (getRandomInt() === 2){
+      else if (getRandomInt() === 2 && numberDesired === true){
       randomPass += randomNumber()}
 
-    else if (getRandomInt() === 3){
-      randomPass += randomSymbol()}
-  }
-
-};
-  
-  
-
-console.log(randomPass);
-//var disredSymbols = prompt("Please enter if you desire special characters in the password!")
-
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+      else  
+      randomPass += randomLowercase()
+    }
+    alert(randomPass);
 }
+else 
+alert ("This is not a valid length!");
+
 
 function randomLowercase(){
-  const lowercaseList = "abcdefghijklmnopqrstuvwxyz";
-  return lowercaseList[Math.floor(Math.random() * lowercaseList.length)];  
-  
+  const lowercaseList = "abcdefghijklmnopqrstuvwxyz";  
+  return lowercaseList[Math.floor(Math.random() * lowercaseList.length)];
 };
 
 function randomUppercase(){
   const uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return uppercaseList[Math.floor(Math.random() * uppercaseList.length)];  
-  
 };
 
 function randomNumber(){
   const numbersList = "0123456789";
   return numbersList[Math.floor(Math.random() * numbersList.length)];  
-  
 };
 
 function randomSymbol(){
   const symbolsList = "!@#$%&*_-?/~";
   return symbolsList[Math.floor(Math.random() * symbolsList.length)];  
-  
 };
 
-console.log(randomPass.length)
 
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
